787353d1e198e790172968fa4bd12b38f565a8e5
vladisvit <vladisvit@ukr.net>
2018-11-11 21:02:55 +0200

    Init

---
+++ UnitTestBase/Base.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Text
+Imports Base
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class Base
+    Private Const cnstDotWorld As String = "w.o.r.l.d."
+    <TestMethod()> Public Sub StringBase()
+        Dim oStringBase As StringBase = New StringBase()
+        Assert.AreEqual(Of String)(cnstDotWorld, oStringBase.GetStringAsCharEnumerator())
+        Assert.AreEqual(Of String)(cnstDotWorld, oStringBase.GetStringAsCharForEach())
+    End Sub
+
+    <TestMethod()> Public Sub CollectionBase()
+        Dim oCollectionBase As MyCollectionsBase = New MyCollectionsBase()
+        Dim sWorld As String = String.Empty
+        For Each oChar In oCollectionBase.MyWorld
+            sWorld += oChar
+        Next
+
+        Assert.AreEqual(cnstDotWorld, sWorld)
+
+        oCollectionBase = New MyCollectionsBase("KVN")
+        sWorld = String.Empty
+        For Each oChar In oCollectionBase.MyWorld
+            sWorld += oChar
+        Next
+        Assert.AreEqual("K.V.N.", sWorld)
+    End Sub
+
+End Class
