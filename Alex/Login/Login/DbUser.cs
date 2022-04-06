8ad77684b09069722ff8f57db229f7058f0fbf61
Alex <burzumzerg@gmail.com>
2022-04-06 23:01:13 +0300

    My first commit =) 1.00 ver, First Task

---
+++ Alex/Login/Login/DbUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    class DbUser
+    {
+        static string[] Users = new string[] { "Admin", "MyPass", "User", "upass" };
+        public string Username { get; set; }
+        
+
+        public string Password { get; set; }
+
+        public DbUser(string username)
+        {
+            this.Username = username;
+        }
+
+        public void CheckLogin()
+        {
+            foreach(string login in Users)
+            {
+                if (login == this.Username)
+                {
+                    Console.WriteLine("Welcome, {0}", login);
+                    Console.WriteLine("Please enter your password");
+                    this.Password = (Console.ReadLine());
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Login incorrect. Let's good buy.");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Alex/Login/Login/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your login");
+            DbUser user = new DbUser(Console.ReadLine());
+
+            user.CheckLogin();
+
+            Console.ReadKey();
+        }
+    }
+}
