3e3ed5191647549c72dd577924a2a38e3c539781
NikiBloodyMikky <crazyskeleton.ua@gmail.com>
2022-04-10 14:43:59 +0300

    first project added in Nikita f. - login HW, 10 apr 2022

---
+++ Nikita/LoginNikita/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Program
+{
+    public class LogPasMain
+    {
+        public string Usernick();
+        public string Userpass();
+    }
+    public class DbUser
+    {
+        public static string[] Users = new string[] { "Admin", "MyPass", "User", "upass" };
+    }
+    static void Main()
+    {
+        Console.WriteLine("Hello fellow, ");
+
+        Console.WriteLine("Please enter your login");
+        var UsNi = new DbUser.Users;
+        UsNi = Console.ReadLine();
+        Console.WriteLine($"Your username is: {UsNi}");
+
+        Console.WriteLine("And your password");
+        var UsPs = new LogPasMain.Userpass();
+        UsPs = Console.ReadLine();
+        Console.WriteLine($"Your password is: {UsPs}");
+    }
+}
