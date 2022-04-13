a1c28f46aacbff97f5c98a267de41a3ef5a3c98b
NikiBloodyMikky <crazyskeleton.ua@gmail.com>
2022-04-13 19:29:56 +0300

    Added validation loading screen; improved login success and failure

--- Nikita/LoginNikita/Program.cs
+++ Nikita/LoginNikita/Program.cs
@@ -1,59 +1,83 @@
 ﻿using System;
 
-public class Program
+namespace LoginProgram
 {
-    internal class LogPasMain
+    public class Program
     {
-        public string Usernick;
-        public string Userpass;
-    }
-
-    internal class DbUser
-    {
-        public static string[] Users = new string[] { "Admin", "MyPass", "User", "upass" };
-    }
-
-    internal class UserValidation
-    {
-        public bool Validation(LogPasMain user)
+        internal class LogPasMain
         {
-            for (int i = 0; i < DbUser.Users.Length; i++)
+            public string Usernick;
+            public string Userpass;
+        }
+
+        internal class DbUser
+        {
+            public static string[] Users = new string[] { "Admin", "MyPass", "User", "upass", "Nikita", "Kniazkov" };
+        }
+
+        internal class UserValidation
+        {
+            public bool Validation(LogPasMain user)
             {
-                if (user.Usernick.Equals(DbUser.Users[i]) && user.Userpass.Equals(DbUser.Users[++i]))
+                for (int i = 0; i < DbUser.Users.Length; i++)
                 {
-                    return true;
+                    if (user.Usernick.Equals(DbUser.Users[i]) && user.Userpass.Equals(DbUser.Users[++i]))
+                    {
+                        return true;
+                    }
                 }
+                return false;
             }
-            return false;
         }
-    }
 
-    static void Main()
-    {
-        var user = new LogPasMain(); //create an object 'user' of LogPasMain 
-        Console.WriteLine("Hello fellow, ");
-
-        Console.WriteLine("Please enter your login");
-
-        var userNickName = Console.ReadLine();
-        Console.WriteLine($"Your username is: {userNickName}");
-
-        Console.WriteLine("And your password");
-        var userPassword = Console.ReadLine();
-        Console.WriteLine($"Your password is: {userPassword}");
-        user.Usernick = userNickName ?? string.Empty;
-        user.Userpass = userPassword ?? string.Empty;
-
-        var userValidation = new UserValidation(); //create an object 'userValidation' of UserValidaion type
-        if (userValidation.Validation(user))
+        static void Main()
         {
-            Console.BackgroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("Login!");
-        }
-        else
-        {
-            Console.BackgroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("Login failed");
+            LoginStep:
+            var user = new LogPasMain(); //create an object 'user' of LogPasMain 
+            Console.WriteLine("Hello fellow, ");
+
+            Console.WriteLine("Please enter your login");
+
+            var userNickName = Console.ReadLine();
+            Console.WriteLine($"Your username is: {userNickName}");
+
+            Console.WriteLine("And your password");
+            var userPassword = Console.ReadLine();
+            Console.WriteLine($"Your password is: {userPassword}");
+            user.Usernick = userNickName ?? string.Empty;
+            user.Userpass = userPassword ?? string.Empty;
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            static void TimerCallback(Object o) => Console.Write(".");
+            Console.Write("PLEASE WAIT");
+            Timer t = new Timer(TimerCallback, null, 0, 1000);
+            Thread.Sleep(6000);
+            t.Dispose();
+            Console.Clear();
+            Console.ResetColor();
+
+            var userValidation = new UserValidation(); //create an object 'userValidation' of UserValidaion type
+            if (userValidation.Validation(user))
+            {
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Login success!");
+                Console.ResetColor();
+                Console.WriteLine("Welcome back! Press enter to exit.");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Login failed...");
+                Console.WriteLine("Please, try again.");
+                while (Console.KeyAvailable) Console.ReadKey(true);
+                Console.ReadKey();
+                Console.ResetColor();
+                Console.Clear();
+                goto LoginStep;
+            }
+            while (Console.KeyAvailable) Console.ReadKey(true);
+            Console.ReadKey();
         }
     }
 }
